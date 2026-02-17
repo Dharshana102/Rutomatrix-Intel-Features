e5dbcb4c583c80e1cdd156c58239612bf3cd408d
Dharshana102 <dharshanashri.sivasethupathi@tessolve.com>
2026-02-17 17:30:54 +0530

    Update Bios_serial_log_test.sh
    Changed the dynamic username and path

--- Bios_serial_log/Bios_serial_log_test.sh
+++ Bios_serial_log/Bios_serial_log_test.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-
+# -------------------------------------------------
+# Detect Rutomatrix username dynamically
+# -------------------------------------------------
+if [ -f /boot/firmware/rutomatrix_user ]; then
+    USERNAME=$(cat /boot/firmware/rutomatrix_user)
+elif [ -f /boot/rutomatrix_user ]; then
+    USERNAME=$(cat /boot/rutomatrix_user)
+else
+    USERNAME=$(ls -1 /home | grep -v root | head -n1)
+fi
+ 
+if [ -z "$USERNAME" ]; then
+    echo "ERROR: Could not detect Rutomatrix user"
+    exit 1
+fi
+ 
+BASE_DIR="/home/$USERNAME"
+LOGDIR="$BASE_DIR/serial_logs"
 # Folder where logs will be stored
-LOGDIR="/home/rpi/serial_logs"
-
 # Create the folder if it doesnâ€™t exist
 mkdir -p "$LOGDIR"
 
